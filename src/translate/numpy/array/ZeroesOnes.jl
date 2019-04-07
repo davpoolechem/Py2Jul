da61ef3c5c5c4947e9d23f28b6fc6e1689bfb779
david <david@localhost.localdomain>
2019-04-07 13:37:32 -0500

    Add Numpy _-like array creation operations to translation list

--- src/translate/numpy/array/ZeroesOnes.jl
+++ src/translate/numpy/array/ZeroesOnes.jl
@@ -21,13 +21,25 @@ using GetElements
 function translate_empty(file::Array{String,1})
     for i in 1:length(file)
         if (occursin(r"numpy.empty(.*)",file[i]))
-            regex = match(r"numpy.empty(.*)",file[i])
+            regex::RegexMatch = match(r"numpy.empty(.*)",file[i])
 
-            first_num, second_num = GetElements.two(regex[1])
-            first_num = first_num[2:end]
-            second_num = first_num[1:end]
+            first_dim, second_dim = GetElements.two(regex[1])
+            first_dim = first_dim[2:end]
+            second_dim = first_dim[1:end]
 
-            file[i] = replace(file[i],r"numpy.empty(.*)" => "Matrix(undef,$first_num,$second_num)")
+            file[i] = replace(file[i],r"numpy.empty(.*)" => "fill(undef,($first_dim,$second_dim))")
+        end
+    end
+end
+
+function translate_empty_like(file::Array{String,1})
+    for i in 1:length(file)
+        if (occursin(r"numpy.empty_like(.*)",file[i]))
+            regex = match(r"numpy.empty_like(.*)",file[i])
+
+            array = GetElements.one(regex[1])
+
+            file[i] = replace(file[i],r"numpy.empty_like(.*)" => "fill(undef,(size($array)[1],size($array)[2]))")
         end
     end
 end
@@ -64,20 +76,52 @@ end
 
 function translate_ones(file::Array{String,1})
     for i in 1:length(file)
-        if (occursin("numpy.ones",file[i]))
-            file[i] = replace(file[i],"numpy.ones" => "ones")
-            file[i] = replace(file[i],"((" => "(")
-            file[i] = replace(file[i],"))" => ")")
+        if (occursin(r"numpy.ones(.*)",file[i]))
+            regex::RegexMatch = match(r"numpy.ones(.*)",file[i])
+
+            first_dim, second_dim = GetElements.two(regex[1])
+            first_dim = first_dim[2:end]
+            second_dim = first_dim[1:end]
+
+            file[i] = replace(file[i],r"numpy.ones(.*)" => "fill(1,($first_dim,$second_dim))")
+        end
+    end
+end
+
+function translate_ones_like(file::Array{String,1})
+    for i in 1:length(file)
+        if (occursin(r"numpy.ones_like(.*)",file[i]))
+            regex = match(r"numpy.ones_like(.*)",file[i])
+
+            array = GetElements.one(regex[1])
+
+            file[i] = replace(file[i],r"numpy.ones_like(.*)" => "fill(1,(size($array)[1],size($array)[2]))")
         end
     end
 end
 
 function translate_zeros(file::Array{String,1})
     for i in 1:length(file)
-        if (occursin("numpy.zeros",file[i]))
-            file[i] = replace(file[i],"numpy.zeros" => "zeros")
-            file[i] = replace(file[i],"((" => "(")
-            file[i] = replace(file[i],"))" => ")")
+        if (occursin(r"numpy.zeros(.*)",file[i]))
+            regex::RegexMatch = match(r"numpy.zeros(.*)",file[i])
+
+            first_dim, second_dim = GetElements.two(regex[1])
+            first_dim = first_dim[2:end]
+            second_dim = first_dim[1:end]
+
+            file[i] = replace(file[i],r"numpy.zeros(.*)" => "fill(0,($first_dim,$second_dim))")
+        end
+    end
+end
+
+function translate_zeros_like(file::Array{String,1})
+    for i in 1:length(file)
+        if (occursin(r"numpy.zeros_like(.*)",file[i]))
+            regex = match(r"numpy.zeros_like(.*)",file[i])
+
+            array = GetElements.one(regex[1])
+
+            file[i] = replace(file[i],r"numpy.zeros_like(.*)" => "fill(0,(size($array)[1],size($array)[2]))")
         end
     end
 end
@@ -96,13 +140,30 @@ function translate_full(file::Array{String,1})
     end
 end
 
+function translate_empty_like(file::Array{String,1})
+    for i in 1:length(file)
+        if (occursin(r"numpy.full_like(.*)",file[i]))
+            regex = match(r"numpy.full_like(.*)",file[i])
+
+            array, number = GetElements.two(regex[1])
+            number = first_dim[1:end]
+
+            file[i] = replace(file[i],r"numpy.full_like(.*)" => "fill($number,(size($array)[1],size($array)[2]))")
+        end
+    end
+end
+
 @inline function run(file::Array{String,1})
     translate_empty(file)
+    translate_empty_like(file)
     translate_eye(file)
     translate_identity(file)
     translate_ones(file)
+    translate_ones_like(file)
     translate_zeros(file)
+    translate_zeros_like(file)
     translate_full(file)
+    translate_full_like(file)
 end
 export run
 
