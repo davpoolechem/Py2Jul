9bb23524fc10713bdcc06f439b17bbd6047e5ceb
david <david@localhost.localdomain>
2019-04-13 09:26:26 -0500

    Small changes and additions to random module

--- src/translate/random/ForFloat.jl
+++ src/translate/random/ForFloat.jl
@@ -4,20 +4,28 @@ using GetElements
 
 function translate_random(file::Array{String,1})
     for i in 1:length(file)
-        if (occursin("random.random()",file[i]))
-            file[i] = replace(file[i],"random.random()" => "rand()")
+        if (occursin(r"[\w+]\.random()",file[i]))
+            file[i] = replace(file[i],r"[\w+]\.random()" => "rand()")
         end
     end
 end
 
 function translate_uniform(file::Array{String,1})
     for i in 1:length(file)
-        if (occursin(r"random.uniform(.*)",file[i]))
-            numbers = GetElements.get_elements(r"random.uniform(.*)",file[i])
+        if (occursin(r"[\w+]\.uniform(.*)",file[i]))
+            numbers = GetElements.get_elements(r"[\w+]\.uniform(.*)",file[i])
 
             first_num = numbers[1]
             second_num = numbers[2]
-            file[i] = replace(file[i],r"random.uniform(.*)" => "rand(Distributions.Uniform($first_num,$second_num))")
+            file[i] = replace(file[i],r"[\w+]\.uniform(.*)" => "rand(Distributions.Uniform($first_num,$second_num))")
+        end
+    end
+end
+
+function translate_expovariate(file::Array{String,1})
+    for i in 1:length(file)
+        if (occursin(r"[\w+]\.expovariate(.*)",file[i]))
+            file[i] = replace(file[i],r"[\w+]\.expovariate(.*)" => "randexp(1.0)")
         end
     end
 end
@@ -26,6 +34,7 @@ end
 @inline function run(file::Array{String,1})
     translate_random(file)
     translate_uniform(file)
+    translate_expovariate(file)
 end
 export run
 
